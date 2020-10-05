f87157bd509e88b71591cc747f4c51dfde3743f9
joshijoshi10@gmail.com <joshijoshi10@gmail.com>
2020-10-05 20:10:29 +0530

    jenkins file added

---
+++ batch.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Dell\SelProject
+mvn test
---
+++ src/test/java/org/Academy/forJenkins.java
@@ -0,0 +1,11 @@
+package org.Academy;
+
+import org.testng.annotations.Test;
+
+public class forJenkins {
+
+	@Test
+	public void JenkinsTest() {
+		System.out.println("jenkins test");
+	}
+}
